ffc842a9b79666fe28cd74b6db15c202958d15b2
Ben Sparks <bensparks0@gmail.com>
2021-04-10 16:33:42 +0200

    User input and basic variable types

--- elixirtut.ex
+++ elixirtut.ex
@@ -2,4 +2,28 @@ defmodule M do
   def hello do
     IO.puts "Hello World"
   end
+
+  def main do
+    data_stuff()
+  end
+
+  def user_input do
+    # getting user input
+    name = IO.gets("What is your name?") |> String.trim
+    IO.puts "Hello #{name}!"
+  end
+
+  def var_types do
+    my_int = 123
+    my_float = 45.69
+    one_to_ten = 1..10
+
+    IO.puts "Integer #{is_integer(my_int)}"
+    IO.puts "Float #{is_float(my_float)}"
+    IO.puts "Atom #{is_atom(:Pittsburgh)}"
+    IO.puts "Atom #{is_atom(:"New York")}"
+    # IO.puts "Range #{is_range(one_to_ten)}"
+  end
+
+
 end
